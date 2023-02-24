cf9bd1ab9718155cd42f1fedbf3c30cb9c2936b8
GustavoDSCruz <gustavodiasdsc@gmail.com>
2023-02-24 17:31:30 -0300

    first commit

---
+++ resources/views/contato.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<body>
+<a href="{{url ('/')}}">Voltar para Home</a><br>
+<a href="{{ url ('/produtos')}}">Link para ir para Produtos</a><br>
+<a href="{{ url ('/contato')}}">Link para ir para Contato</a><br>
+<a href="{{ url ('/empresa')}}">Link para ir para Empresa</a>  
+
+</body>
+
+</html>
